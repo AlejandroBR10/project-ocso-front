30795605d2ffbb266736e3a6284e38917e95546c
AlejandroBR10 <alejabaldera@gmail.com>
2025-03-21 09:01:15 -0600

    Cambios en sidebar y middleware

--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import axios from "axios";
 import { TOKEN_NAME } from "@/constants";
+import { div } from "framer-motion/client";
 const CountPage = async () => {
     const userCookies = cookies();
     const token = userCookies.get(TOKEN_NAME)?.value;
@@ -11,7 +12,7 @@ const CountPage = async () => {
     });
     
     const cantidad = countLocations?.data?.length;
-    return `Hay ${cantidad}  tienda${cantidad>1 ? "s" : ""}`;
+    return <div className="w-2/12">{`Hay: ${cantidad}  tienda${cantidad>1 ? "s" : ""}`}</div>;
 }
 
 export default CountPage;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,10 @@ import { div } from "framer-motion/client";
 export default function DashboardPage(){
     return (
         <>
-        <div className="h-full w-1/2 bg-red-100">
+        <div className="h-full w-4/12 bg-red-100">
         <p>Hola soy Section 1</p>
         </div>
-        <div className="h-full w-2/6 bg-red-100">
+        <div className="h-full w-5/12 bg-red-100">
         <p>Hola soy Section 2</p>
         </div>
         </>
