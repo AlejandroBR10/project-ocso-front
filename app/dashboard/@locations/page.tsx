7be3cca09c8520bca571586e835826ec415a1666
AlejandroBR10 <alejabaldera@gmail.com>
2025-03-22 03:20:09 -0600

    feat: Implemententing location card

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { TOKEN_NAME } from "@/constants";
 import { Location } from "@/entities";
 import SelectLocation from "./_components/SelectLocation";
+import LocationCard from "./_components/LocationCard";
 
 const LocationPage = async ({searchParams}: {
   searchParams: { [key: string]: string | string[] | undefined};
@@ -34,6 +35,9 @@ const LocationPage = async ({searchParams}: {
         <div className="w-full flex flex-col items-center h-[90vh] bg-red-50">
             <div className="w-1/2 my-10">
             <SelectLocation locations={data} store={searchParams?.store}/>
+            </div>
+            <div className="w-8/12">
+              <LocationCard store={searchParams.store}/>
             </div>
       </div>
     </div>
