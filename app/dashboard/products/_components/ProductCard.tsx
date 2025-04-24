55913ffb026314aed86c1f4161200287561da227
AlejandroBR10 <alejabaldera@gmail.com>
2025-04-23 18:52:27 -0600

    feat: Barra de Navegacion products

---
+++ app/dashboard/products/_components/ProductCard.tsx
@@ -0,0 +1,17 @@
+import { Product } from "@/entities";
+import { Card, CardBody, CardHeader, Divider } from "@heroui/react";
+
+export default function ProductCard({product} : {product : Product}){
+    return (
+        <Card >
+            <CardHeader>
+            {product.productName}    
+            </CardHeader>
+            <Divider/>
+            <CardBody>
+                <p>Nombre del Producto:<b>{product.productName}</b></p>
+                <p>Precio del Producto:<b>{product.price}</b></p>
+            </CardBody>
+        </Card>
+    )
+}
